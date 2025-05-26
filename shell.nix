e2adec86b7813097db5d9e1b859a6c3fc0acdbb0
Rucadi <ruben.cano96@gmail.com>
2025-05-26 11:14:18 +0000

    add "shell.nix" with an strange environment :)

---
+++ shell.nix
@@ -0,0 +1,28 @@
+let
+  import_specific_commit =
+    commit:
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
+    }) { };
+  git_nixpkgs = import_specific_commit "21808d22b1cda1898b71cf1a1beb524a97add2c4";
+  python3_nixpkgs = import_specific_commit "42acb5dc55a754ef074cb13e2386886a2a99c483";
+  nodejs_nixpkgs = import_specific_commit "c0c50dfcb70d48e5b79c4ae9f1aa9d339af860b4";
+  clang_nixpkgs = import_specific_commit "21808d22b1cda1898b71cf1a1beb524a97add2c4";
+  stdenv_nixpkgs = import_specific_commit "21808d22b1cda1898b71cf1a1beb524a97add2c4";
+in
+stdenv_nixpkgs.mkShell {
+  name = "environment";
+  buildInputs = [
+    git_nixpkgs.git
+    python3_nixpkgs.python3
+    nodejs_nixpkgs.nodejs-7_x
+    clang_nixpkgs.llvmPackages_15.clang
+  ];
+  shellHook = ''
+    # show all programs versions
+    echo "git version: $(git --version)"
+    echo "python3 version: $(python3 --version)"
+    echo "nodejs version: $(node --version)"
+    echo "clang version: $(clang --version)"
+  '';
+}
--- shell/shell.nix
+++ shell/shell.nix
@@ -1,49 +1,51 @@
-{pkgs ? import <nixpkgs> { }}:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 pkgs.mkShell {
-          buildInputs = with pkgs; [
-            boost
-            sqlite
-            xz
-            gcc
-            gnumake
-            jq
-          ];
-          
-          shellHook =''
-            echo "C++ Integration Demo Development Environment"
-            echo "Dependencies: Boost, SQLite, and XZ"
-            echo "link attrs: -lboost_system -lsqlite3 -llzma -lstdc++fs"
-            echo "Build command: $CXX -std=c++17 src/main.cpp -o integration_demo -lboost_system -lsqlite3 -llzma"
-            echo "Run command: ./integration_demo"
-            echo "Docker load: docker load -i $(nix build .#docker_image --print-out-paths)"
-            function sbom_lic() {
-            
-              jq -r '[.metadata.component.purl, (.components[] | .purl + "\n" + (.licenses[0].license.id // "NO LICENSE FOUND"))] | join("\n")' $1
-            }
+  buildInputs = with pkgs; [
+    boost
+    sqlite
+    xz
+    gcc
+    gnumake
+    jq
+  ];
 
-            function sbom_deps(){
-              jq -r '[.metadata.component.purl, (.components[] | .purl)] | join("\n")' $1
-            }
+  shellHook = ''
+    echo "C++ Integration Demo Development Environment"
+    echo "Dependencies: Boost, SQLite, and XZ"
+    echo "link attrs: -lboost_system -lsqlite3 -llzma -lstdc++fs"
+    echo "Build command: $CXX -std=c++17 src/main.cpp -o integration_demo -lboost_system -lsqlite3 -llzma"
+    echo "Run command: ./integration_demo"
+    echo "Docker load: docker load -i $(nix build .#docker_image --print-out-paths)"
+    function sbom_lic() {
 
-            function build()
-            {
-              $CXX -std=c++17 src/main.cpp \
-                -o integration_demo \
-                -lboost_system \
-                -lsqlite3 \
-                -llzma \
-                -lstdc++fs
-            }
+      jq -r '[.metadata.component.purl, (.components[] | .purl + "\n" + (.licenses[0].license.id // "NO LICENSE FOUND"))] | join("\n")' $1
+    }
 
-            function run()
-            {
-              ./integration_demo
-            }
+    function sbom_deps(){
+      jq -r '[.metadata.component.purl, (.components[] | .purl)] | join("\n")' $1
+    }
 
-            function clean()
-            {
-              rm -f integration_demo
-            }
+    function build()
+    {
+      $CXX -std=c++17 src/main.cpp \
+        -o integration_demo \
+        -lboost_system \
+        -lsqlite3 \
+        -llzma \
+        -lstdc++fs
+    }
 
-          '';
-        }
+    function run()
+    {
+      ./integration_demo
+    }
+
+    function clean()
+    {
+      rm -f integration_demo
+    }
+
+  '';
+}
