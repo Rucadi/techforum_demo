f1a6a996bb75c15f9f20c4547129185bdadbb1d3
Rucadi <ruben.cano96@gmail.com>
2025-05-26 14:53:19 +0000

    add grype

--- shell/shell.nix
+++ shell/shell.nix
@@ -9,6 +9,7 @@ pkgs.mkShell {
     gcc
     gnumake
     jq
+    grype
   ];
 
   shellHook = ''
@@ -17,7 +18,9 @@ pkgs.mkShell {
     echo "link attrs: -lboost_system -lsqlite3 -llzma -lstdc++fs"
     echo "Build command: $CXX -std=c++17 src/main.cpp -o integration_demo -lboost_system -lsqlite3 -llzma"
     echo "Run command: ./integration_demo"
-    echo "Docker load: docker load -i $(nix build .#docker_image --print-out-paths)"
+    echo "Docker load: docker load -i $(nix build .#docker_image --no-link --print-out-paths 2>/dev/null)"
+    echo "Check vulnerabilities: grype --add-cpes-if-none $(nix build .#sbom --no-link --print-out-paths 2>/dev/null)"
+
     function sbom_lic() {
 
       jq -r '[.metadata.component.purl, (.components[] | .purl + "\n" + (.licenses[0].license.id // "NO LICENSE FOUND"))] | join("\n")' $1
